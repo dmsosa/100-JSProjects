44620bc1003deefb1b630e58499f178a9826c327
Durian Sosa <dmsosa21@outlook.com>
2024-07-15 20:32:32 +0100

    spriessen wachsen

--- src/components/Plant.jsx
+++ src/components/Plant.jsx
@@ -3,16 +3,48 @@ function Plant() {
 
     return  (
         <div className="flower-container">
-            <div className="flower">
-                <div className="flower__blatter">
-                    <div className="blute"></div>
-                    <div className="blatt blatt--1"></div>
-                    <div className="blatt blatt--2"></div>
-                    <div className="blatt blatt--3"></div>
-                    <div className="blatt blatt--4"></div>
+            <div className="blume blume--1">
+                <div className="blume__blatter">
+                    <div className="blume__blatt blume__blatt--1"></div>
+                    <div className="blume__blatt blume__blatt--2"></div>
+                    <div className="blume__blatt blume__blatt--3"></div>
+                    <div className="blume__blatt blume__blatt--4"></div>
+                    <div className="weiss-kreis"></div>
+                    {/* licht */}
                 </div>
-                <div className="flower__stengel"></div>
+                <div className="blume__stengel">
+                    <div className="blume__stengel__blatt blume__stengel__blatt--1"></div>
+                </div>
             </div>
+            <div className="lange_spriess lange_spriess--1">
+                <div className="wachsen-ans" style={{animationDelay: "4s"}}>
+                    <div className="spriess spriess--0"></div>
+                </div>
+                <div className="wachsen-ans" style={{animationDelay: "4.4s"}}>
+                    <div className="spriess spriess--1"></div>
+                </div>
+                <div className="wachsen-ans" style={{animationDelay: "4.1s"}}>
+                    <div className="spriess spriess--2"></div>
+                </div>
+                <div className="wachsen-ans" style={{animationDelay: "4.7s"}}>
+                    <div className="spriess spriess--3"></div>
+                </div>
+            </div>
+            <div className="lange_spriess lange_spriess--2">
+                <div className="wachsen-ans" style={{animationDelay: "2.4s"}}>
+                    <div className="spriess spriess--0"></div>
+                </div>
+                <div className="wachsen-ans" style={{animationDelay: "2.7s"}}>
+                    <div className="spriess spriess--1"></div>
+                </div>
+                <div className="wachsen-ans" style={{animationDelay: "2.1s"}}>
+                    <div className="spriess spriess--2"></div>
+                </div>
+                <div className="wachsen-ans" style={{animationDelay: "2.2s"}}>
+                    <div className="spriess spriess--3"></div>
+                </div>
+            </div>
+            
         </div>
     )
 
--- src/pages/FlowerPage.jsx
+++ src/pages/FlowerPage.jsx
@@ -1,19 +1,23 @@
+import { useEffect } from "react";
 import Plant from "../components/Plant";
 import { useGod } from "../context/GodContext";
 
 function FlowerPage() {
     const { withGod } = useGod();
     
+    useEffect(() => {
+         
+    }, [])
     return (
-        <div className="page">
+        <div className="page flower-page">
+            <div className="god-toggler">God toggler</div>
             <div className="container">
                 <div className="row">
-                    <div className="col">
+                    <div className="col col-12">
                         <h1>Today is Jdss's Day</h1>
                         <p>This is your very very happy birthday!</p>
                     </div>
                     <div className="col">
-                        <p>{withGod ? "White ": "No god"}</p>
                     </div>
                 </div>
                 <div className="row">
